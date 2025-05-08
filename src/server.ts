8625bbd61cfffcb900525234bb5317760e240f11
Lucy Villogas <lvillogas.training@innevo.cl>
2025-05-08 18:02:25 -0500

    server

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,13 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://ecommerce-floreria-frontend.vercel.app',
+    'http://localhost:5173'
+  ],
+  credentials: true
+}));
 app.use(morgan('dev'));
 app.use(compression());
 app.use(express.json({limit: '50mb'}));
